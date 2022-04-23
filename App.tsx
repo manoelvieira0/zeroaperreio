49e6cd2082b1749231a299ddc7feece9a1c58981
netovieira01_ <netinhovieira1801@gmail.com>
2022-04-23 14:28:25 -0300

    Projeto com Expo e Typescript adicionado

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Manoel Vieira dos Santos Neto</Text>
+      <Text>Projeto Iniciado!</Text>
       <StatusBar style="auto" />
     </View>
   );
