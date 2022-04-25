d3e58469d3a7eab2b483922348e3681fd57c9461
netovieira01_ <netinhovieira1801@gmail.com>
2022-04-25 15:20:59 -0300

    Posicionando os cartões em destaque

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
 import { Feather } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import {getStatusBarHeight} from 'react-native-iphone-x-helper'
 
 export const Container = styled.View`
     flex: 1;
@@ -13,7 +14,7 @@ export const Header = styled.View`
 
     background-color: ${({ theme }) => theme.colors.primary};
 
-    align-items: center;
+    align-items: flex-start;
     justify-content: center;
     flex-direction: row;
 `;
@@ -21,6 +22,8 @@ export const Header = styled.View`
 export const UserWrapper = styled.View`
     width: 100%;
     padding: 0 24px;
+    margin-top: ${getStatusBarHeight()+ RFValue(28)}px;
+
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
@@ -61,6 +64,10 @@ export const Icon = styled(Feather)`
 
 export const HighlightCards = styled.ScrollView.attrs({
     horizontal: true,
-    showsHorizontalScrollIndicator: true,
+    showsHorizontalScrollIndicator: false,
     contentContainerStyle: {paddingLeft: 24}
-})``;
+})`
+    width: 100%;
+    position: absolute;
+    margin-top: ${RFPercentage(20)}px;
+`;
