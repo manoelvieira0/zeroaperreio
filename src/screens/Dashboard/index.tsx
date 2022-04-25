69a1ceccb77e043fa59147fe4f3dfd1de910be72
netovieira01_ <netinhovieira1801@gmail.com>
2022-04-25 16:28:28 -0300

    Tipando e acessando propriedades no Styled Components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -31,10 +31,10 @@ export function Dashboard() {
                 </UserWrapper>
             </Header>
 
-            <HighlightCards horizontal={true} showsHorizontalScrollIndicator={false} contentContainerStyle={{paddingLeft: 24}}>
-                <HighlightCard />
-                <HighlightCard />
-                <HighlightCard />
+            <HighlightCards>
+                <HighlightCard type="up" title="Entradas" amount="R$ 17.400,00" lastTransaction="Última entrada dia 13 de abril"/>
+                <HighlightCard type="down" title="Saídas" amount="R$ 1.259,00" lastTransaction="Última saída dia 03 de abril"/>
+                <HighlightCard type="total" title="Entradas" amount="R$ 16.141,00" lastTransaction="01 à 16 de abril"/>
             </HighlightCards>
         </Container>
     )
