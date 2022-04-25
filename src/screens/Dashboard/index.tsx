631642036a070450c5304dc7649295174008c6d3
netovieira01_ <netinhovieira1801@gmail.com>
2022-04-25 14:27:40 -0300

    Criação dos Cartões de Destaque

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
 
 import {
     Container,
@@ -29,6 +30,7 @@ export function Dashboard() {
                 </UserWrapper>
             </Header>
 
+            <HighlightCard />
         </Container>
     )
 }
