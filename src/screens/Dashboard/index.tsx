3c8f0f4143bf21571a02f2228b1e96a13df07068
netovieira01_ <netinhovieira1801@gmail.com>
2022-04-25 13:58:37 -0300

    Utilizando Ícones

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import {
     Container,
     Header,
@@ -8,6 +9,7 @@ import {
     User,
     UserGreeting,
     UserName,
+    Icon
 } from './styles'
 
 export function Dashboard() {
@@ -23,6 +25,7 @@ export function Dashboard() {
                             <UserName>Manoel</UserName>
                         </User>
                     </UserInfo>
+                    <Icon name="power"/>
                 </UserWrapper>
             </Header>
 
