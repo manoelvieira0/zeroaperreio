b407875d2d670592c109ece7690dd408e279015d
netovieira01_ <netinhovieira1801@gmail.com>
2022-05-16 09:31:02 -0300

    Utilizando a Tab Navigation

--- App.tsx
+++ App.tsx
@@ -10,7 +10,9 @@ import {
 } from '@expo-google-fonts/poppins'
 
 import theme from './src/global/styles/theme'
-import { Register } from './src/screens/Register';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,7 +25,9 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
